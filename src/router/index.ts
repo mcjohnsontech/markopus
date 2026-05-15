62e374bc70ea06e67e5e0bff5f05cb9ff940b328
Mcjohnsontech <mcj26017@gmail.com>
2026-05-15 21:12:04 +0100

    set Vue-router up for gh-pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { createWebHashHistory } from 'vue-router'
 import { defineAsyncComponent } from 'vue'
 
 const DashboardPage = defineAsyncComponent(() => import('@/pages/DashboardPage.vue'))
@@ -8,7 +9,7 @@ const AlertsPage = defineAsyncComponent(() => import('@/pages/AlertsPage.vue'))
 const SettingsPage = defineAsyncComponent(() => import('@/pages/SettingsPage.vue'))
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
