c96b1a3f145a69e22bf4c155a402d3838962d723
Mcjohnsontech <mcj26017@gmail.com>
2026-05-15 18:21:58 +0100

    Make it work

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/markopus/', // Must match your GitHub repo name
   plugins: [
     vue(),
     vueJsx(),
